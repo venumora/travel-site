c5bb7acbc276508ccac10081a5b7cdfe5791d0df
Venu Mora <venukumar.mora@gmail.com>
2017-07-24 11:15:17 -0700

    lesson 26 section 10

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp'),
-	postcss = require('gulp-postcss'),
-	autoprefixer = require('autoprefixer'),
-	cssvars = require('postcss-simple-vars'),
-	postcssnested = require('postcss-nested'),
-	postcssimport = require('postcss-import'),
-	mixins = require('postcss-mixins');
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-		   .pipe(postcss([postcssimport, mixins, postcssnested, cssvars, autoprefixer]))
-		   .on('error', function(errorInfo) {
-		   		console.log(errorInfo.toString());
-		   		this.emit('end');
-		   })
-		   .pipe(gulp.dest('./app/temp/styles'));
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,26 +1,27 @@
 var gulp = require('gulp'),
-	watch = require('gulp-watch'),
-	browser = require('browser-sync').create();
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
+gulp.task('watch', function() {
 
-gulp.task('watch', function(){
-	browser.init({
-		notify: false,
-		server: {
-			baseDir: 'app'
-		}
-	});
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
 
-	watch('./app/index.html', function(){
-		browser.reload();
-	});
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
 
-	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('cssInject');
-	});
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
-	gulp.src('.app/temp/styles/styles.css')
-	.pipe(browser.stream());
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
